dc8fc17e7c37355f985014d55f6c9bf54052a018
Het <hra.opash@gmail.com>
2026-01-21 18:12:35 +0530

    .env

--- src/api/GetAPIData.jsx
+++ src/api/GetAPIData.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const getMoviesData = async () => {
   try {
     const response = await fetch(
-      "http://www.omdbapi.com/?i=tt3896198&apikey=d0a07b20&s=titanic&page=1",
+      `http://www.omdbapi.com/?i=tt3896198&apikey=${import.meta.env.VITE_API_KEY}&s=titanic&page=1`,
     );
     const data = await response.json();
     return data;
